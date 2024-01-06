3f8c67826ba381e33d04344575404f58395a0ccf
SylviaAchieng <achiengsylvia157@gmail.com>
2024-01-06 15:55:21 +0300

    first commit

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php include ('partials-front/menu.php'); ?>
+<div class="container">
+<h1 class="text-center">phone</h1>
+    0769743898
+    <br><br>
+    OR <BR></BR>
+    0745322903
+    </div>
+
+
+
+</div>
+<div class="clearfix"></div>
+    
+
+<?php include ('partials-front/footer.php'); ?>
---
+++ partials-front/footer.php
@@ -0,0 +1,13 @@
+
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Sudies Group members</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
